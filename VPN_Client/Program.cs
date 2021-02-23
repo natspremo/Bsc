4cb61bc32e685323291ad9ac6155551134d866f4
Natasa Spremo <natasa-spremo@hotmail.com>
2021-02-23 13:50:14 +0100

    project end

---
+++ VPN_Client/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VPN_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect("192.168.0.18", 8001); // use the ipaddress as   in the server program
+
+                Console.WriteLine("Connected");
+                while (true)
+                {
+
+                    Console.Write("Send vpn request? : Y/N [x - exit]");
+
+                    String str = Console.ReadLine();
+                    if (str.ToLower().Equals("n"))
+                        continue;
+
+                    Stream stm = tcpclnt.GetStream();
+
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    byte[] ba = asen.GetBytes(str);
+                    Console.WriteLine("Transmitting.....");
+
+                    stm.Write(ba, 0, ba.Length);
+
+                    byte[] bb = new byte[100];
+                    int k = stm.Read(bb, 0, 100);
+                    string recv = "";
+                    for (int i = 0; i < k; i++)
+                        recv += Convert.ToChar(bb[i]);
+                    if (recv.ToLower().Equals("x"))
+                    {
+                        tcpclnt.Close();
+                        break;
+                    }
+                        
+                    Console.WriteLine("VPN IPv4 address: " +recv);
+                    Regex rgx = new Regex(@"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+                    MatchCollection result = rgx.Matches(recv);
+                    if(result.Count != 0)
+                    {
+                        tcpclnt.Close();
+                        OpenVPNClient(recv);
+                    }
+                        
+                }
+                
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.Message);
+            }
+        }
+
+        public static void OpenVPNClient(string ipAdr)
+        {
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect(ipAdr, 8001); 
+
+                Console.WriteLine("Connected");
+                while (true)
+                {
+
+                    Console.Write("Send message [x - exit]");
+
+                    String str = Console.ReadLine();
+
+                    Stream stm = tcpclnt.GetStream();
+
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    byte[] ba = asen.GetBytes(str);
+                    Console.WriteLine("Transmitting.....");
+
+                    stm.Write(ba, 0, ba.Length);
+
+                    byte[] bb = new byte[100];
+                    int k = stm.Read(bb, 0, 100);
+                    string recv = "";
+                    for (int i = 0; i < k; i++)
+                        recv += Convert.ToChar(bb[i]);
+                    if (recv.ToLower().Equals("x"))
+                        break;
+                    Console.WriteLine(recv);
+                }
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.Message);
+            }
+        }
+    }
+}
---
+++ VPN_Test/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPN_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPAddress ipAd = IPAddress.Parse("192.168.0.18"); //use local       m/c IP address, and use the same in the client
+
+                /* Initializes the Listener */
+                TcpListener myList = new TcpListener(ipAd, 8001);
+
+                /* Start Listeneting at the specified port */
+                myList.Start();
+
+                Console.WriteLine("The server is running at port 8001...");
+                Console.WriteLine("The local End point is  :" + myList.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection.....");
+
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                while (true)
+                {
+                    byte[] b = new byte[100];
+                    int k = s.Receive(b);
+                    string message = "";
+                    for (int i = 0; i < k; i++)
+                    {
+                        message += Convert.ToChar(b[i]);
+                    }
+                    if (message.ToLower().Equals("x"))
+                    {
+                        ASCIIEncoding asen = new ASCIIEncoding();
+                        s.Send(asen.GetBytes("x"));
+                        break;
+                    }
+                    else
+                        Console.WriteLine("\nRequest for VPN IP address.");
+                    //send ip address
+                    try
+                    {
+                        var vpn = NetworkInterface.GetAllNetworkInterfaces().First(x => x.Name == "TunnelBear");
+                        var ip = vpn.GetIPProperties().UnicastAddresses.First(x => x.Address.AddressFamily == AddressFamily.InterNetwork).Address.ToString();
+                        
+                        ASCIIEncoding asen = new ASCIIEncoding();
+                        s.Send(asen.GetBytes(ip));
+                        Console.WriteLine("\nSent Acknowledgement");
+                        OpenVPNServer(IPAddress.Parse(ip));
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        string str = "VPN is not activated.";
+                        Console.WriteLine(e.Message);
+                        ASCIIEncoding asen = new ASCIIEncoding();
+                        s.Send(asen.GetBytes(str));
+                        Console.WriteLine("\nSent Acknowledgement");
+                    }
+
+                };
+
+                /* clean up */
+                s.Close();
+                myList.Stop();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.Message);
+            }
+
+        }
+        public static void OpenVPNServer(IPAddress ipAdr)
+        {
+            try
+            {
+                /* Initializes the Listener */
+                TcpListener myList = new TcpListener(ipAdr, 8001);
+
+                /* Start Listeneting at the specified port */
+                myList.Start();
+
+                Console.WriteLine("The server on VPN is running at port 8001...");
+                Console.WriteLine("Waiting for a connection.....");
+
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                while (true)
+                {
+                    byte[] b = new byte[100];
+                    int k = s.Receive(b);
+                    string message = "";
+                    for (int i = 0; i < k; i++)
+                    {
+                        message += Convert.ToChar(b[i]);
+                    }
+                    if (message.ToLower().Equals("x"))
+                    {
+                        ASCIIEncoding asen = new ASCIIEncoding();
+                        s.Send(asen.GetBytes("x"));
+                        break;
+                    }
+                    else
+                        Console.WriteLine(message);
+                    
+                    try
+                    {
+                        string ack = "Message succesfully transmitted.";
+                        ASCIIEncoding asen = new ASCIIEncoding();
+                        s.Send(asen.GetBytes(ack));
+                        Console.WriteLine("\nSent Acknowledgement");
+                    }
+                    catch (Exception e)
+                    {
+                        string str = "Acknowledgment wasn't sent due to error: ";
+                        Console.WriteLine(str + e.Message);
+                    }
+                };
+
+                /* clean up */
+                s.Close();
+                myList.Stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.Message);
+            }
+        }
+    }
+}
